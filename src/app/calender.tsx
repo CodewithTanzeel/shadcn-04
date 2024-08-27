fd53817cfc4479ff819214433bd2f314e63d5ab7
Tanzeel0713 <Tanzeelofficial@outlook.com>
2024-08-27 21:43:13 +0500

    update

---
+++ src/app/calender.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Calendar } from "@/components/ui/calendar"
+
+
+
+const CalenderDemo = () => {
+    const [date, setDate] = React.useState<Date | undefined>(new Date())
+  return (
+    <div>
+
+  <Calendar
+    mode="single"
+    selected={date}
+    onSelect={setDate}
+    className="rounded-md border"
+  />
+
+
+    </div>
+
+  )
+}
+
+export default CalenderDemo
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,10 @@ export default function Home() {
   return (
     <div>
       <h1 className="text-center text-3xl pt-5">Shadcn ui</h1>
-      <div className="grid"></div>
+      <div className="grid grid-cols-2 gap-4 p-5 m-7">
+        <div className="bg-blue-500 h-5">Heading</div>
+        <div className="bg-red-600 h-5">Calender</div>
+      </div>
     </div>
   );
 }
