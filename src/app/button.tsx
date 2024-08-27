252a6ee54d875e6e0a12c38deed3b5caf321dbd6
Tanzeel0713 <Tanzeelofficial@outlook.com>
2024-08-27 22:23:09 +0500

    Update

---
+++ src/app/button.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from "@/components/ui/button"
+
+
+const ButtonDemo = ({ text }: any | string) => {
+    return (
+        <div><Button variant="outline">{text}</Button>
+        </div>
+    )
+}
+
+export default ButtonDemo
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,25 @@
-
 import Image from "next/image";
-import CalenderDemo from "./calender";
 import TypoDemo from "./typography";
+import ButtonDemo from "./button";
+import CalenderDemo from "./calender";
 
 export default function Home() {
   return (
     <div>
       <h1 className="text-center text-3xl pt-5">Shadcn ui</h1>
+       {/* Main Div */}
       <div className="grid grid-cols-2 gap-4 p-5 m-7">
-        {/* Heading div */}
-        <div className="flex justify-center p-5 items-center "><TypoDemo/></div>
+        {/* heading div */}
+        <div className="flex flex-col gap-8 justify-center items-center p-5">
+          <TypoDemo/>
+          <div className="flex justify-center items-center gap-5">
+            <ButtonDemo text={"Search"}/>
+            <ButtonDemo text={"Profile"}/>
+          </div>
+        </div>
         {/* CalenderDiv */}
-        <div className="justify-center items-center p-5 flex"> <CalenderDemo />
-
+        <div className="flex justify-center items-center p-5">
+          <CalenderDemo/>
         </div>
       </div>
     </div>
